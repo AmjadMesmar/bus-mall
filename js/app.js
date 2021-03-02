88e691dbaff05794f2a5a0e120ad0cd0ad5d7b23
Amjad Mesmar <Amjad.Mesmar@gmail.com>
2021-03-02 10:07:08 +0200

    added shown chart view

--- js/app.js
+++ js/app.js
@@ -192,10 +192,12 @@ function renderChart() {
 
   let nameArray = [];
   let clicksArray = [];
+  let shownArray = [];
 
   for (let i = 0; i < busProducts.length; i++) {
     nameArray.push(Bus.all[i].name);
     clicksArray.push(Bus.all[i].clicks);
+    shownArray.push(Bus.all[i].shown);
 
 
   }
@@ -205,52 +207,21 @@ function renderChart() {
     data: {
       labels: nameArray,
       datasets: [{
-        label: Bus.counter,
+        label: 'Displayed',
+        data: shownArray,
+        backgroundColor:
+          'rgba(34, 213, 106, 0.7)',
+        borderColor:
+          'rgba(0, 0, 0, 0.8)',
+        borderWidth: 2
+      },
+      {
+        label: 'Clicked',
         data: clicksArray,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 120, 255, 0.2)',
-          'rgba(255, 169, 64, 0.2)',
-          'rgba(255, 99, 200, 0.2)',
-          'rgba(150, 200, 235, 0.2)',
-          'rgba(200, 200, 200, 0.2)',
-          'rgba(12, 43, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)',
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)',
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)'
-        ],
+        backgroundColor:
+          'rgba(213, 34, 34, 0.7)',
+        borderColor:
+          'rgba(0, 0, 0, 0.8)',
         borderWidth: 2
       }]
     },
